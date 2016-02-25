95f24d4d260e43b636a42efc146f47afb0309eb6
Hans Fjallemark <hans.fjallemark@cignium.com>
2016-02-25 08:07:34 +0100

    enhance error reporting

--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
+import 'source-map-support/register'
 import Koa from 'koa'
 import bodyParser from 'koa-bodyparser'
 import mongoose from 'mongoose'
 import raven from 'raven'
 import routes from './server/routes'
 
+const app = new Koa()
 const sentry = new raven.Client(process.env.SENTRY_DSN, {
   release: process.env.HEROKU_RELEASE_VERSION,
 })
 
-process.on('uncaughtException', error => {
-  console.error(error.stack)
-  sentry.captureException(error)
-  process.exit(1)
-})
-
-const app = new Koa()
-
+sentry.patchGlobal()
 mongoose.connect(process.env.MONGOLAB_URI)
 routes.forEach(route => app.use(route))
 
-app.on('error', sentry.captureException)
+app.on('error', error => sentry.captureException(error))
 app.use(bodyParser())
 app.listen(process.env.PORT || 3000)
